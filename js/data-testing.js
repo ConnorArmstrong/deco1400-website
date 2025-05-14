71269093e7c4fe26f9d54f57ccca739d55ed5d57
ConnorArmstrong <connor.armstrong@pm.me>
2025-05-14 22:48:36 +1000

    JSON data storage testing

---
+++ js/data-testing.js
@@ -0,0 +1,27 @@
+async function getJSON() {
+    const response = await fetch("./media/data.json");
+    const json = await response.json();
+    //console.log("Fetched data:", json);
+    return json;
+}
+
+async function getContentData(contentTitle) {
+    const data = await getJSON();
+    console.log(typeof data);
+
+    const item = data.items.find(item => item.title === contentTitle);
+
+    if (!item) {
+        console.warn("No item found with title: ${contentTitle}");
+        return;
+    }
+
+    console.log(item);
+
+    const contentType = item.contentType;
+
+    console.log(contentType)
+}
+
+
+getContentData("The King in Yellow1");
